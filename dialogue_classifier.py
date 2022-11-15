49d8340898caddb88e457d066ba15f6be3f00822
Amanda Smith <asmith2@bowdoin.edu>
2022-11-15 11:29:36 -0500

    WIP - Defining our classes.

--- dialogue_classifier.py
+++ dialogue_classifier.py
@@ -28,10 +28,24 @@ def preprocess_words(words, stemmer):
         stems.add(stemmer.stem(word)) #note: come back here when done because the stemmer should only create stems for verbs not other parts of speech 
     return stems
 
+def organize_raw_training_data(raw_training_data, stemmer):
+    all_characters = []
+    for key in raw_training_data.keys(): 
+        character_words = ()
+        unique_words = set()
+        for sentence in raw_training_data[key]: 
+            sentence = sentence.split(" ")
+            for word in sentence: 
+                unique_words.add(word)
+        character_words = (unique_words, key)
+        all_characters.append(character_words)
+    return all_characters
+
 def main(): 
     training_data = get_raw_training_data("dialogue_data.csv")
     stemmer = LancasterStemmer()
-    print(preprocess_words(training_data['galadriel'][0], stemmer))
+    #reprocess_words(training_data['galadriel'][0], stemmer)
+    print(organize_raw_training_data(get_raw_training_data("dialogue_data.csv"), stemmer))
     #print(stemmer.stem("swimming"))
 
 if __name__ == "__main__":
